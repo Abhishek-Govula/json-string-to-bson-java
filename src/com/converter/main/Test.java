c73806d98338ff8c1dcec2643dd7bae1655d473c
Naga Sai Abhishek Govula <Nagasai_Govula@ad.infosys.com>
2016-12-24 15:15:23 +0530

    Closed the opened streams

--- src/com/converter/main/Test.java
+++ src/com/converter/main/Test.java
@@ -10,17 +10,35 @@ import com.converter.Convert;
 public class Test {
 	public static void main(String[] args) {
 		InputStream is = Test.class.getResourceAsStream("/json-data.txt");
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-			String s = "";
-			StringBuffer sb = new StringBuffer();
-			while((s=br.readLine())!=null) {
-				sb.append(s);
+		if(is!=null) {
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+				String s = "";
+				StringBuffer sb = new StringBuffer();
+				while((s=br.readLine())!=null) {
+					sb.append(s);
+				}
+				System.out.println(Convert.createBsonObj(sb.toString()));
+			} catch (IOException e) {
+				System.out.println("Exception while trying to read the file");
+				e.printStackTrace();
+			} finally {
+				if(br!=null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-			System.out.println(Convert.createBsonObj(sb.toString()));
-		} catch (IOException e) {
-			System.out.println("Exception while trying to read the file");
-			e.printStackTrace();
+		} else {
+			System.err.println("Could not load the File specified. Please check the path");
 		}
 	}
 }
